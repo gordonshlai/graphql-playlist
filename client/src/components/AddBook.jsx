9a3b3eb296dd1d4312c3fc7b40bfeb9028382aef
gordonshlai <gordonshlai@yahoo.com.hk>
2021-08-28 23:04:15 +0100

    30. Updating Component State

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { graphql } from "react-apollo";
 
 import { getAuthorsQuery } from "../queries/queries";
 
 const AddBook = ({ data }) => {
+  const [name, setName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [authorId, setAuthorId] = useState("");
+
   const displayAuthors = () =>
     data.loading ? (
       <option disabled>Loading Authors...</option>
@@ -14,21 +19,28 @@ const AddBook = ({ data }) => {
       ))
     );
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(name, "name");
+    console.log(genre, "genre");
+    console.log(authorId, "authorId");
+  };
+
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={submitForm}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </div>
 
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setGenre(e.target.value)} />
       </div>
 
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select onChange={(e) => setAuthorId(e.target.value)}>
           <option>Select author</option>
           {displayAuthors()}
         </select>
